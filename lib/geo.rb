c3e9de83a531cc6edab71cc6431643a1e9f6fba8
yujiloh0816 <imp.yfujiwara@gmail.com>
2020-05-06 21:43:10 +0900

    緯度経度を返すライブラリを作成

---
+++ lib/geo.rb
@@ -0,0 +1,27 @@
+require 'base64'
+require 'json'
+require 'net/https'
+
+module Geo
+  class << self
+    def geocode(address)
+      api_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{address}&key=#{ENV['GOOGLE_CLOUD_API_KEY']}"
+
+      encode_url = URI.encode(api_url)
+      uri = URI.parse(encode_url)
+      https = Net::HTTP.new(uri.host, uri.port)
+      https.use_ssl = true
+      request = Net::HTTP::Get.new(uri.request_uri)
+      request['Content-Type'] = 'application/json'
+      response = https.request(request)
+
+      # APIレスポンス出力
+      body = JSON.parse(response.body)
+      if body['status'] == 'OK'
+        body['results'][0]['geometry']['location'].values
+      else
+        nil
+      end
+    end
+  end
+end
